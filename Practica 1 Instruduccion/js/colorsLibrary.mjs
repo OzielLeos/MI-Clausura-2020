f9f970eefa2419cfdf9184eb7a5e8f3a930dd7c5
Oziel Leos Martinez <37005448+OzielLeos@users.noreply.github.com>
2020-01-24 21:26:19 -0600

    Add files via upload
    Primera Practica de Introduccion

---
+++ Practica 1 Instruduccion/js/colorsLibrary.mjs
@@ -0,0 +1,5 @@
+import { ColorModel } from "./models/colorModel.mjs";
+
+export const red = new ColorModel(1,0,0,0);
+export const blue = new ColorModel(0,0,1,0);
+export const green = new ColorModel(0,1,0,0);
